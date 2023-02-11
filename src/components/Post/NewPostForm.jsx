cad8ad9bc8b410096eb270ce8bb06f6bd6f40450
andrej0609 <andrejus.galkinas@gmail.com>
2023-02-11 16:36:15 +0200

    Prideta validacija

--- src/components/Post/NewPostForm.jsx
+++ src/components/Post/NewPostForm.jsx
@@ -1,30 +1,33 @@
-import PostContext from "../../contexts/PostContext";
-import UserContext from "../../contexts/UserContext";
-import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { nanoid } from "nanoid";
+import { Formik, Form, Field } from 'formik';
+import * as yup from "yup";
+import PostContext from "../../contexts/PostContext";
+import UserContext from "../../contexts/UserContext";
+import { useContext } from "react";
 
 const NewPostForm = () => {
-
-  const [formInputs, setFormInputs] = useState({
-    heading: '',
-    content: ''
-  });
 
   const { addNewPost } = useContext(PostContext);
   const { loggedInUser } = useContext(UserContext);
 
   const navigation = useNavigate();
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const validationSchema = yup.object().shape({
+    heading: yup.string().required('Heading is required').min(3, 'Heading must be at least 3 characters'),
+    content: yup.string().required('Content is required').min(3, 'Content must be at least 3 characters'),
+  });
+
+  const handleSubmit = (values) => {
     const newPost = {
-      heading: formInputs.heading,
-      content: formInputs.content,
+      heading: values.heading,
+      content: values.content,
       id: nanoid(),
       userId: loggedInUser.id,
       likes: 0,
+      likedBy: [],
       dislikes: 0,
+      dislikedBy: [],
       Time: new Date().toLocaleString(),
       edited: false
     };
@@ -36,23 +39,30 @@ const NewPostForm = () => {
   return (
     <>
       <div className="FormAddPost">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Heading:
-            <input type="text" name="heading" required
-              value={formInputs.heading}
-              onChange={(e) => setFormInputs({ ...formInputs, heading: e.target.value })}
-            />
-          </label>
-          <label>
-            Content:
-            <input type="text" name="content" required
-              value={formInputs.content}
-              onChange={(e) => setFormInputs({ ...formInputs, content: e.target.value })}
-            />
-          </label>
-          <input type="submit" value="Create new Post" />
-        </form>
+        <Formik
+          initialValues={{
+            heading: '',
+            content: ''
+          }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <label>
+                Heading:
+                <Field name="heading" type="text" />
+                {errors.heading && touched.heading ? <div>{errors.heading}</div> : null}
+              </label>
+              <label>
+                Content:
+                <Field name="content" type="text" />
+                {errors.content && touched.content ? <div>{errors.content}</div> : null}
+              </label>
+              <input type="submit" value="Create new Post" />
+            </Form>
+          )}
+        </Formik>
       </div>
     </>
   );
