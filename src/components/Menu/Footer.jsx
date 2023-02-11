811c62813a3b7e145f76ba4e081b2488e6ef9daf
andrej0609 <andrejus.galkinas@gmail.com>
2023-02-11 16:37:07 +0200

    Pridetas iframe

--- src/components/Menu/Footer.jsx
+++ src/components/Menu/Footer.jsx
@@ -1,41 +1,56 @@
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="footer__content">
-              <div className="footer__logo">
-                <a href="/">
-                  <img src="https://dt2sdf0db8zob.cloudfront.net/wp-content/themes/websiteplanet/img/wsp-logo-schema.png" alt="logo" />
-                </a>
-              </div>
-              <div className="footer__social">
-                <a href="/">
-                  <i className="fas fa-rss"></i>
-                </a>
-                <a href="/">
-                  <i className="fas fa-envelope"></i>
-                </a>
-                <a href="/">
-                  <i className="fas fa-phone"></i>
-                </a>
-                <a href="/">
-                  <i className="fas fa-map-marker-alt"></i>
-                </a>
-              </div>
-              <div className="footer__menu">
-                <ul>
-                  <li>
-                    <a href="/">Home</a>
-                  </li>
-                  <li>
-                    <a href="/">About</a>
-                  </li>
-                  <li>
-                    <a href="/">Contact</a>
-                  </li>
-                </ul>
+    <>
+      <hr  className="line2"/>
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="footer__content">
+                <div className="footer__logo">
+                  <a href="/">
+                    <img src="https://dt2sdf0db8zob.cloudfront.net/wp-content/themes/websiteplanet/img/wsp-logo-schema.png" alt="logo" />
+                  </a>
+                </div>
+                <div className="footer__menu">
+                  <ul>
+                    <li>
+                      <a href="/">Home</a>
+                    </li>
+                    <li>
+                      <a href="/">About</a>
+                    </li>
+                    <li>
+                      <a href="/">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="footer__social">
+                  <a href="/">
+                    <i className="fas fa-rss"> RSS</i>
+                  </a>
+                  <a href="/">
+                    <i className="fas fa-envelope"> Email</i>
+                  </a>
+                  <a href="/">
+                    <i className="fas fa-phone"> Phone</i>
+                  </a>
+                  <a href="/">
+                    <i className="fas fa-map-marker-alt"> Address</i>
+                  </a>
+                </div>
+                <div className="footer__map">
+                  <div className="mapouter">
+                    <div className="gmap_canvas">
+                      <iframe
+                        width="300"
+                        height="200"
+                        title="map"
+                        src="https://maps.google.com/maps?q=Vilnius&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="footer__copy">
                 <p>
@@ -45,8 +60,8 @@ const Footer = () => {
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
