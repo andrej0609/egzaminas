61e0ac951263ceb5d1916dd9531ff81f5273e91d
andrej0609 <andrejus.galkinas@gmail.com>
2023-02-11 16:35:26 +0200

    Pakeisti Like/Dislike contextai

--- src/contexts/AnswerContext.jsx
+++ src/contexts/AnswerContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useState } from "react";
 import { useEffect } from "react";
 import Monkey from "../components/img/monkey.gif"
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
 const AnswerContext = createContext();
 
@@ -8,6 +10,7 @@ const AnswerProvider = ({ children }) => {
 
   const [answers, setAnswers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { loggedInUser } = useContext(UserContext);
 
   useEffect(() => {
     async function fetchData() {
@@ -53,29 +56,46 @@ const AnswerProvider = ({ children }) => {
   }
 
   const likeAnswer = async (id) => {
-    const answer = answers.find(answer => answer.id === id);
-    const updatedAnswer = { ...answer, likes: answer.likes + 1 };
-    await fetch(`http://localhost:5000/atsakymas/${id}`, {
-      method: 'PATCH',
-      body: JSON.stringify(updatedAnswer),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    setAnswers(answers.map(answer => answer.id === id ? { ...answer, likes: answer.likes + 1 } : answer));
-  }
+    const answer = answers.find((answer) => answer.id === id);
+    if (answer.likedBy.includes(loggedInUser.id)) {
+      const res = await fetch(`http://localhost:5000/atsakymas/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ likes: answer.likes - 1, likedBy: answer.likedBy.filter(userId => userId !== loggedInUser.id) }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const updatedData = await res.json();
+      setAnswers(answers.map(answer => answer.id.toString() === id ? { ...answer, ...updatedData } : answer));
+    } else {
+      const res = await fetch(`http://localhost:5000/atsakymas/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ likes: answer.likes + 1, likedBy: [...answer.likedBy, loggedInUser.id] }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const updatedData = await res.json();
+      setAnswers(answers.map(answer => answer.id.toString() === id ? { ...answer, ...updatedData } : answer));
+    }
+  };
 
   const dislikeAnswer = async (id) => {
-    const answer = answers.find(answer => answer.id === id);
-    const updatedAnswer = { ...answer, dislikes: answer.dislikes - 1 };
-    await fetch(`http://localhost:5000/atsakymas/${id}`, {
-      method: 'PATCH',
-      body: JSON.stringify(updatedAnswer),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    setAnswers(answers.map(answer => answer.id === id ? { ...answer, dislikes: answer.dislikes - 1 } : answer));
-  }
-
+    const answer = answers.find((answer) => answer.id === id);
+    if (answer.dislikedBy.includes(loggedInUser.id)) {
+      const res = await fetch(`http://localhost:5000/atsakymas/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ dislikes: answer.dislikes - 1, dislikedBy: answer.dislikedBy.filter(userId => userId !== loggedInUser.id) }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const updatedData = await res.json();
+      setAnswers(answers.map(answer => answer.id.toString() === id ? { ...answer, ...updatedData } : answer));
+    } else {
+      const res = await fetch(`http://localhost:5000/atsakymas/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ dislikes: answer.dislikes + 1, dislikedBy: [...answer.dislikedBy, loggedInUser.id] }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const updatedData = await res.json();
+      setAnswers(answers.map(answer => answer.id.toString() === id ? { ...answer, ...updatedData } : answer));
+    }
+  };
 
   return (
     <AnswerContext.Provider
